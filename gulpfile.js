c9d37e3ddc2de330fba8e863335495de533c07d5
Kacper Makowka <kacper.makowka@redoffice.int>
2020-04-06 15:57:27 +0100

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+const gulp = require("gulp");
+const jshint = require("gulp-jshint");    
+const sass = require("gulp-sass");
+const sourcemaps = require("gulp-sourcemaps");
+const browserSync = require("browser-sync").create();
+
+gulp.task("jshint", function () {
+    return gulp.src("./js/*.js")           
+        .pipe(jshint())
+        .pipe(jshint.reporter("default"))
+});
+
+gulp.task("sass", function () {
+    return gulp.src("scss/main.scss")
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: 'expanded',
+        }).on("error", sass.logError))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest("css"))
+        .pipe(browserSync.stream())
+});
+
+gulp.task("watch", function () {
+    browserSync.init({
+        server:{
+            baseDir:'./'
+        }
+    })
+    gulp.watch("scss/**/*.scss", gulp.series("sass"));
+    gulp.watch("./*.html").on('change', browserSync.reload);
+    gulp.watch("./js/*.js").on('change', browserSync.reload);
+    gulp.watch("./*.js").on('change', browserSync.reload);
+});
